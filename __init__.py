5f8aa3cb54db10141d91fa84f9473372bec6a21e
sschleis <sebastian.schleis@gmail.com>
2025-08-03 23:06:31 +0200

    show

--- __init__.py
+++ __init__.py
@@ -41,17 +41,38 @@ class ShowText:
             "required": {
                 "text": ("STRING", {"forceInput": True}),
             },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
         }
 
-    RETURN_TYPES = ()
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
     FUNCTION = "show"
     OUTPUT_NODE = True
-    CATEGORY = "MyCustomNodes"
+    OUTPUT_IS_LIST = (True,)
 
-    def show(self, text):
-        print(f"[ShowText] Received text: {text}", file=sys.stderr)
-        print(f"[ShowText] Type of text: {type(text)}", file=sys.stderr)
-        return {"ui": {"text": list(text)}}
+    def show(self, text, unique_id=None, extra_pnginfo=None):
+        if unique_id is not None and extra_pnginfo is not None:
+            if not isinstance(extra_pnginfo, list):
+                print("Error: extra_pnginfo is not a list")
+            elif (
+                not isinstance(extra_pnginfo[0], dict)
+                or "workflow" not in extra_pnginfo[0]
+            ):
+                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
+            else:
+                workflow = extra_pnginfo[0]["workflow"]
+                node = next(
+                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
+                    None,
+                )
+                if node:
+                    node["widgets_values"] = [text]
+
+        return {"ui": {"text": text}, "result": (text,)}
+
 
 NODE_CLASS_MAPPINGS = {
     "AddNumbers": AddNumbers,
