02b4fda775065ac405f10203b936e425ce849f79
sschleis <sebastian.schleis@gmail.com>
2025-08-03 22:09:58 +0200

    first commit

---
+++ addNode.py
@@ -0,0 +1,22 @@
+import torch
+
+class AddNumbers:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "a": ("FLOAT", {"default": 0.0}),
+                "b": ("FLOAT", {"default": 0.0}),
+            }
+        }
+
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "add"
+    CATEGORY = "MyCustomNodes"
+
+    def add(self, a, b):
+        return (a + b,)
+
+NODE_CLASS_MAPPINGS = {
+    "AddNumbers": AddNumbers
+}
